ae8212c07665e7fc4665393777c9e62efe4ed6d5
Bronco Bill <janusoo@gmail.com>
2026-01-25 13:34:31 -0500

    Installed USWDS npm packages, adjusted some custom settings, installed cutom fonts, and ran gulp init

--- web/themes/custom/uswds_compile_subtheme_exp_japan/gulpfile.js
+++ web/themes/custom/uswds_compile_subtheme_exp_japan/gulpfile.js
@@ -14,11 +14,11 @@ uswds.settings.version = 3;
  * Set as many as you need - see https://designsystem.digital.gov/documentation/getting-started/developers/phase-two-compile/#step-5-customize-path-settings
  */
 uswds.paths.dist.css = './css';
-uswds.paths.dist.sass = './sass';
-uswds.paths.dist.theme = './sass';
-uswds.paths.dist.img = './assets/uswds/img';
-uswds.paths.dist.fonts = './assets/uswds/fonts';
-uswds.paths.dist.js = './assets/uswds/js';
+uswds.paths.dist.sass = './scss';
+uswds.paths.dist.theme = './scss';
+uswds.paths.dist.img = './assets/img';
+uswds.paths.dist.fonts = './assets/fonts';
+uswds.paths.dist.js = './assets/js';
 
 /**
  * Exports
